45942fc4079af3b17b0db55fd454de79e11b1a06
Donnovan-hub <santiagosalas942@gmail.com>
2024-10-30 20:48:55 -0500

    Alfinnnnnnnnnn

---
+++ Veterinaria_/Views_Menu_Admin/Registrar_Tipo_Consulta.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Veterinaria_.Views_Menu_Admin
+{
+    /// <summary>
+    /// Lógica de interacción para Registrar_Tipo_Consulta.xaml
+    /// </summary>
+    public partial class Registrar_Tipo_Consulta : UserControl
+    {
+        public Registrar_Tipo_Consulta()
+        {
+            InitializeComponent();
+        }
+    }
+}
